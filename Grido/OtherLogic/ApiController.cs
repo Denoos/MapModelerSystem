fa36bf7a3c86d6f2f558d3ea2046715657fbc1b1
Denoos <denoosdenoos@gmail.com>
2025-03-20 12:33:07 +1000

    Коментарии в API контроллере

--- Grido/OtherLogic/ApiController.cs
+++ Grido/OtherLogic/ApiController.cs
@@ -18,12 +18,12 @@ namespace Grido.OtherLogic
         // => _client = new() { BaseAddress = new Uri("") }; //НУЖНО добавить сюда адрес апи и раскоментить
 
         User User;
-        public async Task<bool> Registration(User user)
+        public async Task<bool> Registration(User user) //отправляет в апи, и возвращает 1 если user ==  декод от респонса else 0
         {
             User = user;
             return true;
         }
-        public async Task<User> Authorisation(User user)
+        public async Task<User> Authorisation(User user) //отправляет в апи, и возвращает декод от респонса
         {
             return User;
         }
