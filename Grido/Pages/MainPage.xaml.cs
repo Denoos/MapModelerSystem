250afa9f7cbabc43b4e1d13a50f203ab5faff905
Denoos <denoosdenoos@gmail.com>
2025-03-20 14:14:04 +1000

    нормальный PropertyChanged

--- Grido/Pages/MainPage.xaml.cs
+++ Grido/Pages/MainPage.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using Grido.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,8 +22,13 @@ namespace Grido.Pages
     /// <summary>
     /// Логика взаимодействия для MainPage.xaml
     /// </summary>
-    public partial class MainPage : Page
+    public partial class MainPage : Page, INotifyPropertyChanged
     {
+        private Visibility forAdminsVis;
+        private Visibility forUsersVis;
+
+        public Visibility ForUsersVis { get => forUsersVis; set { forUsersVis = value; Signal(); } }
+        public Visibility ForAdminsVis { get => forAdminsVis; set { forAdminsVis = value; Signal(); } }
         public MainPage(MainWindow mv, User user)
         {
             InitializeComponent();
@@ -31,5 +39,21 @@ namespace Grido.Pages
             InitializeComponent();
             DataContext = this;
         }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void Signal([CallerMemberName]string prop = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        //                      _____
+        //                     |  |  |
+        //                     |__|__|
+        //                     |     |
+        //                     |     |
+        //                     |     |
+        //                     |     | вот так правилно еблан
+        //                _____|     |_____
+        //               |                 |
+        //               |                 |
+        //               |_________________|
     }
 }
