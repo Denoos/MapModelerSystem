f9463995084c30cbf715ef9e9f3fe4240b108736
Denoos <denoosdenoos@gmail.com>
2025-03-24 09:58:14 +1000

    Главная вкладка закончена

--- Grido/Pages/MainPage.xaml.cs
+++ Grido/Pages/MainPage.xaml.cs
@@ -17,6 +17,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Net.WebRequestMethods;
 
 namespace Grido.Pages
 {
@@ -32,7 +33,7 @@ namespace Grido.Pages
         private User User { get => user; set { user = value; Signal(); } }
         public Visibility ForUsersVis { get => forUsersVis; set { forUsersVis = value; Signal(); } }
         public Visibility ForAdminsVis { get => forAdminsVis; set { forAdminsVis = value; Signal(); } }
-        ApiController api = ApiController.Inst;
+        ApiController api = ApiController.Inst; //пока что не используется, но это нужно.
 
         public MainPage(MainWindow mv, User user)
         {
@@ -63,28 +64,18 @@ namespace Grido.Pages
             => mv.CurrentPage = new WelcomePage(mv);
 
         private void NotSure_Click(object sender, RoutedEventArgs e)
-        {
-            tabber.SelectedItem = mainTab;
-        }
+            => tabber.SelectedItem = mainTab;
 
         private void LikeRazrab_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
+            => MessageBox.Show("Вы поддержали разработчика!", "Спасибо!");
+        
         private void Help_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
+            => MainTextblock.Text = "Помощь в пользовании приложения:\nСлева расположены вкладки, чем больше у вас прав, тем больше доступно возможностей.\nДля повышения доступа - авторизуйтесь. Для более подробной информации рекомендуется прочитать документацию на GitHub. \n \n Почта для makin_cat@gmail.com";
 
         private void GitHub_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
+            => MainTextblock.Text = "https://github.com/Denoos/MapModelerSystem";
 
         private void InfoRazrab_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
+            => MainTextblock.Text = "Разработчик проекта: Denoos";
     }
 }
