89d197a7ba3fa9e2645d8ea56da761650e3e3d73
AA <Student@CAB4-2>
2025-04-03 14:26:38 +1000

    влдорпв

--- Grido/Pages/FormMapPage.xaml.cs
+++ Grido/Pages/FormMapPage.xaml.cs
@@ -65,18 +65,10 @@ namespace Grido.Pages
 
         private async void Save_Click(object sender, RoutedEventArgs e)
         {
-            if (CheckForValidContent())
-            {
-                if (IsAdd)
-                    await api.AddMap(Map);
-                else await api.EditMap(Map);
-                Cancel_Click(sender, e);
-            }
-        }
-
-        private bool CheckForValidContent()
-        {
-            return true;
+            if (IsAdd)
+                await api.AddMap(Map);
+            else await api.EditMap(Map);
+            Cancel_Click(sender, e);
         }
     }
 }
